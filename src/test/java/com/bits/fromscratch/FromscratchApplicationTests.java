d418eecb75725522087065bee2f8cbd63d6b7096
Sudip Sarker <sudipdiu@gmail.com>
2021-09-16 04:00:43 +0600

    initial commit

---
+++ src/test/java/com/bits/fromscratch/FromscratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.fromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromscratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
